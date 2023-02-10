0fe906d5c32e13d3fec346ae67102fc4ee04d589
Sarthak <sarda.sarthak03@gmail.com>
2023-02-11 02:57:01 +0530

    removed hints

--- Blackjack capstone proj.py
+++ Blackjack capstone proj.py
@@ -19,21 +19,6 @@ import art
 ## Cards are not removed from the deck as they are drawn.
 ## The computer is the dealer.
 
-##################### Hints #####################
-
-#Hint 1: Go to this website and try out the Blackjack game: 
-#   https://games.washingtonpost.com/games/blackjack/
-#Then try out the completed Blackjack project here: 
-#   http://blackjack-final.appbrewery.repl.run
-
-#Hint 2: Read this breakdown of program requirements: 
-#   http://listmoz.com/view/6h34DJpvJBFVRlZfJvxF
-#Then try to create your own flowchart for the program.
-
-#Hint 3: Download and read this flow chart I've created: 
-#   https://drive.google.com/uc?export=download&id=1rDkiHCrhaf9eX7u7yjM1qwSuyEk-rPnt
-
-#Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
 def deal_card():
   cards =[11,2,3,4,5,6,7,8,9,10,10,10,10]
   card= random.choice(cards)
